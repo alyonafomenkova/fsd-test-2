d3f73be89a0b58af19d4013f3ca9fa4f0ff0336a
Alena Fomenkova <fruitella30sep@mail.ru>
2020-04-26 00:23:40 +0300

    Adding dropdown. WIP.

---
+++ src/components/dropdown/dropdown.js
@@ -0,0 +1,30 @@
+import './dropdown.scss';
+import '../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
+
+jQuery(function() {
+  $('.iqdropdown').iqDropdown({
+    // max total items
+    maxItems: Infinity,
+    // min total items
+    minItems: 0,
+    // text to show on the dropdown override data-selection-text attribute
+    selectionText: 'item',
+    // text to show for multiple items
+    textPlural: 'items',
+    // optionally can use setSelectionText function to override selectionText
+    setSelectionText: (itemCount, totalItems) => { /* return string */ },
+    // buttons to increment/decrement
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter'
+    },
+    // fires when an item quantity changes
+    onChange: (id, count, totalItems) => {},
+    // return false to prevent an item decrement
+    beforeDecrement: (id, itemCount) => {},
+    // return false to prevent an item increment
+    beforeIncrement: (id, itemCount) => {}
+  });
+});
--- src/pages/uikit/form-elements/form-elements.js
+++ src/pages/uikit/form-elements/form-elements.js
@@ -1,1 +1,2 @@
 import "./form-elements.scss";
+import '../../../components/dropdown/dropdown.js';
