fb1a7e0c99f7f20212b987f2c1dd6289c33b348e
Alena Fomenkova <fruitella30sep@mail.ru>
2020-04-28 00:16:05 +0300

    Adding dropdown.

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,17 +2,44 @@ import './dropdown.scss';
 import '../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 
 jQuery(function() {
-  $('.iqdropdown').iqDropdown({
-    // max total items
+  const menu = $('.iqdropdown');
+  const clearButton = $('.iqdropdown-menu-buttons__clear');
+  const $this = $(this);
+  const setPlural = (count) => {
+    if (count === 1) {
+      return 'гость';
+    } else if (count >= 2 && count <= 4) {
+      return 'гостя';
+    } else if (count === 0 || count >= 5) {
+      return 'гостей';
+    }
+  };
+
+  const options = {
     maxItems: Infinity,
-    // min total items
     minItems: 0,
-    // text to show on the dropdown override data-selection-text attribute
-    selectionText: 'item',
-    // text to show for multiple items
-    textPlural: 'items',
-    // optionally can use setSelectionText function to override selectionText
-    setSelectionText: (itemCount, totalItems) => { /* return string */ },
+    setSelectionText: (itemCount, totalItems) => {
+      if (menu.hasClass('menu-open')) {
+        if (totalItems > 0) {
+          clearButton.css({'visibility': 'visible'});
+        } else {
+          clearButton.css({'visibility': 'hidden'});
+        }
+        return totalItems + ' ' + setPlural(totalItems);
+      }
+    },
+    onChange: (id, count, totalItems) => {
+      if (totalItems > 0) {
+        clearButton.click((event) => {
+          event.preventDefault();
+          count = 0;
+          totalItems = 0;
+          console.log("CLEAR", id, count, totalItems);
+        });
+      }
+      console.log("onChange: ", id, count, totalItems);//
+      //this.setSelectionText(totalItems);
+    },
     // buttons to increment/decrement
     controls: {
       position: 'right',
@@ -20,11 +47,24 @@ jQuery(function() {
       controlsCls: 'iqdropdown-item-controls',
       counterCls: 'counter'
     },
-    // fires when an item quantity changes
-    onChange: (id, count, totalItems) => {},
-    // return false to prevent an item decrement
-    beforeDecrement: (id, itemCount) => {},
-    // return false to prevent an item increment
-    beforeIncrement: (id, itemCount) => {}
-  });
+  };
+
+  $('.iqdropdown').iqDropdown(options);
+
+  $this.off('click').on('click', $this.click((event) => {
+      console.log("$this: ", $this);//
+      //event.preventDefault();
+    console.log("event.target: ", event.target);
+    const cond1 = $(event.target).hasClass('iqdropdown-selection');
+    const cond2 = $(event.target).hasClass('iqdropdown-menu-buttons__apply');
+      if (cond1 || cond2) {
+        console.log("toggleClass: ", event.target);
+        //$this.toggleClass('menu-open');
+      }
+    }));
+
+  //});
+
+
+  //console.log("totalItems: ", options.totalItems);
 });
