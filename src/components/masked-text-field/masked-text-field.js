f3f5aa38bc71566fc4c6e240bddb6dadbefa5375
Alena Fomenkova <fruitella30sep@mail.ru>
2020-04-30 19:14:40 +0300

    Refactoring of masked-text-field.

--- src/components/masked-text-field/masked-text-field.js
+++ src/components/masked-text-field/masked-text-field.js
@@ -1,16 +1,6 @@
 import './masked-text-field.scss';
 import 'jquery-mask-plugin';
 
-
 jQuery(function() {
-  console.log("MASK");
   $('.masked-text-field_date').mask('00.00.0000');
-  // $('.masked-text-field_date').mask('00.00.0000', {
-  //   translation: {
-  //     'Z': {
-  //       pattern: /[0-9]/,
-  //       optional: true
-  //     }
-  //   }
-  // });
 });
