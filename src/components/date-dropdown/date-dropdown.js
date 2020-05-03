e35a205cca8a7f4872bd0d47e8b1b72183417839
Alena Fomenkova <fruitella30sep@mail.ru>
2020-05-03 19:14:57 +0300

    Adding range dates for date dropdown.

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -2,9 +2,9 @@ import './date-dropdown.scss';
 import '../../../node_modules/air-datepicker/dist/js/datepicker.min.js';
 
 jQuery(function() {
-  const $start = $('#start');
-  const $end = $('#end');
-  const $filter = $('#filter');
+  const $start = $('#start1-id');
+  const $end = $('#end1-id');
+  const $filter = $('#filter1-id');
   const baseOptions = {
     startDate: new Date(),
     prevHtml: '<button class="buttons datepicker--nav-prev" type="button"><span>arrow_back</span></button>',
@@ -16,23 +16,25 @@ jQuery(function() {
     },
     clearButton: true,
     autoClose: false,
+    range: true,
+    multipleDatesSeparator: ' - ',
   };
   const additionalOptionsForStartInput = {
-    onSelect: function (formattedDate, date) {
+    onSelect: function (fd, d, picker) {
       const $applyButton = $start.datepicker().data('datepicker').$datepicker.find('.datepicker--button[data-action="apply"]');
-      $end.data('datepicker').update('minDate', date);
-      $applyButton.click(() => {
-        event.preventDefault();
-        event.stopPropagation();
-        $start.datepicker().data('datepicker').hide();
-        $end.focus();
-      })
+      $start.val('');
+      $end.val('');
+        $applyButton.click(() => {
+          event.preventDefault();
+          event.stopPropagation();
+          $start.datepicker().data('datepicker').hide();
+          $start.val(fd.split('-')[0]);
+          $end.val(fd.split('-')[1]);
+        })
     }
   };
   const additionalOptionsForFilterInput = {
-    range: true,
     dateFormat: 'dd M',
-    multipleDatesSeparator: ' - ',
     onSelect: function (formattedDate, date) {
       const $applyButton = $filter.datepicker().data('datepicker').$datepicker.find('.datepicker--button[data-action="apply"]');
       $applyButton.click(() => {
@@ -43,13 +45,16 @@ jQuery(function() {
     }
   };
   const additionalOptionsForEndInput = {
-    onSelect: function (formattedDate, date) {
+    onSelect: function (fd, d, picker) {
       const $applyButton = $end.datepicker().data('datepicker').$datepicker.find('.datepicker--button[data-action="apply"]');
-      $start.data('datepicker').update('maxDate', date);
+      $start.val('');
+      $end.val('');
       $applyButton.click(() => {
         event.preventDefault();
         event.stopPropagation();
         $end.datepicker().data('datepicker').hide();
+        $start.val(fd.split('-')[0]);
+        $end.val(fd.split('-')[1]);
       })
     }
   };
@@ -62,6 +67,8 @@ jQuery(function() {
     const options = $.extend({}, baseOptions, additionalOptions);
     return options;
   };
+
+  $start.addClass('datepicker-here');
   $start.datepicker(createOptions(additionalOptionsForStartInput));
   $end.datepicker(createOptions(additionalOptionsForEndInput));
   $filter.datepicker(createOptions(additionalOptionsForFilterInput));
