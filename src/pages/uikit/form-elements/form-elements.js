300e7fe32ee56be1c0e43f784afa3c363077d800
Alena Fomenkova <fruitella30sep@mail.ru>
2020-05-04 17:47:23 +0300

    Adding radio component.

--- src/pages/uikit/form-elements/form-elements.js
+++ src/pages/uikit/form-elements/form-elements.js
@@ -4,3 +4,4 @@ import '../../../components/date-dropdown/date-dropdown.js';
 import '../../../components/masked-text-field/masked-text-field.js';
 import '../../../components/subscription-text-field/subscription-text-field.js';
 import '../../../components/checkbox-buttons/checkbox-buttons.js';
+import '../../../components/radio-buttons/radio-buttons.js';
