21d87249d444a7b4588cf4433412f5d9795fa674
Alena Fomenkova <fruitella30sep@mail.ru>
2020-05-04 16:30:21 +0300

    Adding checkbox component.

--- src/pages/uikit/form-elements/form-elements.js
+++ src/pages/uikit/form-elements/form-elements.js
@@ -3,3 +3,4 @@ import '../../../components/dropdown/dropdown.js';
 import '../../../components/date-dropdown/date-dropdown.js';
 import '../../../components/masked-text-field/masked-text-field.js';
 import '../../../components/subscription-text-field/subscription-text-field.js';
+import '../../../components/checkbox-buttons/checkbox-buttons.js';
