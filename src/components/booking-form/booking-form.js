cf79d89a2e5857bda725fc03805d5fb47086b9d7
Alena Fomenkova <fruitella30sep@mail.ru>
2020-05-14 16:02:54 +0300

    Adding booking form.

---
+++ src/components/booking-form/booking-form.js
@@ -0,0 +1,4 @@
+import './booking-form.scss';
+import '../date-dropdown/date-dropdown.js';
+import '../dropdown/dropdown.js';
+import '../buttons/buttons.js';
--- src/components/login-form/login-form.js
+++ src/components/login-form/login-form.js
@@ -1,6 +1,1 @@
 import './login-form.scss';
-// import '../text-field/text-field.js';
-// import '../radio-buttons/radio-buttons.js';
-// import '../masked-text-field/masked-text-field.js';
-// import '../toggle-button/toggle-button.js';
-// import '../buttons/buttons.js';
