5c39458049ee226a2fe7403bd1965cdf5a2c0f8e
Alena Fomenkova <fruitella30sep@mail.ru>
2020-06-10 21:49:26 +0300

    Refactoring.

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,4 +1,7 @@
 import './search-form.scss';
 import '../button/button.js';
-import '../date-dropdown/date-dropdown.js';
 import '../dropdown/dropdown.js';
+import DateDropdown from '../date-dropdown/date-dropdown.js';
+
+const dateDropdownList = document.querySelectorAll('.js-date-dropdown');
+dateDropdownList.forEach((dateDropdown) => new DateDropdown(dateDropdown).init());
