a7a4e882d7704f30e7dc565a628c2dfd68f8b523
Alena Fomenkova <fruitella30sep@mail.ru>
2020-09-04 22:07:11 +0300

    Close expandable-checkbox when clicking on document #7.

--- src/components/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -4,10 +4,24 @@ jQuery(function() {
   const EXPANDED_CLASS = 'expandable-checkbox_state_expanded';
   const selection = $('.js-expandable-checkbox__selection');
 
+  function handleDocumentClick(evt) {
+    const target = evt.target.closest('.js-expandable-checkbox__selection') || evt.target.closest('.checkbox_type_expandable');
+    if (target) {
+      return;
+    } else {
+      const expandedCheckboxes = document.querySelectorAll('.expandable-checkbox_state_expanded');
+      expandedCheckboxes.forEach((checkbox) => {
+        checkbox.classList.remove('expandable-checkbox_state_expanded');
+      });
+    }
+  }
+
   selection.click(function() {
     const target = $(event.target).closest('.js-expandable-checkbox');
     const checkboxList = $(target.find('.js-checkbox'));
     target.toggleClass(EXPANDED_CLASS);
     target.hasClass(EXPANDED_CLASS) ? checkboxList.show() : checkboxList.hide();
   });
+
+  $(document).click(handleDocumentClick);
 });
