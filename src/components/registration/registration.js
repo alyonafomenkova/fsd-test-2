afbdb73e8f03969e239e5b282cc0e2647e80a3b9
Alena Fomenkova <fruitella30sep@mail.ru>
2020-09-08 20:11:20 +0300

    Changing names according to BEM.

--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -3,4 +3,4 @@ import '../masked-text-field/masked-text-field.js';
 import '../radio-buttons/radio-buttons.js';
 import '../text-field/text-field.js';
 import '../toggle-button/toggle-button.js';
-import './registration-form.scss';
+import './registration.scss';
