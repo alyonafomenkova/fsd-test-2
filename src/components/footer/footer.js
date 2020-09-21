5f666c5cca6e7cc931171fd8dcb07cad4d0e1364
Alena Fomenkova <fruitella30sep@mail.ru>
2020-09-21 16:23:17 +0300

    Removing duplicate code #13.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import '../button/button.js';
 import '../logo/logo.js';
+import '../social-list/social-list.js';
 import '../subscription-text-field/subscription-text-field.js';
 import './footer.scss';
